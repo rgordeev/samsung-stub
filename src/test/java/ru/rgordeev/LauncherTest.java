cdc8235db31b8e8f701740dc3079a8d3860e3189
Roman Gordeev <roma.gordeev@gmail.com>
2019-10-31 17:08:09 +0300

    add trivial test

---
+++ src/test/java/ru/rgordeev/LauncherTest.java
@@ -0,0 +1,13 @@
+package ru.rgordeev;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LauncherTest {
+
+    @Test
+    public void SimpleTest() {
+        Assert.assertTrue(true);
+    }
+
+}
