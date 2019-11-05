1351211d27f9285b63d6bf576b3ac91b4fd53cba
rgordeev <roma.gordeev@gmail.com>
2019-11-05 20:27:24 +0300

    task 1.7.5

---
+++ src/main/java/ru/rgordeev/Task175.java
@@ -0,0 +1,20 @@
+package ru.rgordeev;
+
+import java.util.Scanner;
+
+public class Task175 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(count(a));
+    }
+
+    private static int count(int a) {
+        int acc = 0;
+        while (a > 0) {
+            acc = acc + 1;
+            a = a / 2;
+        }
+        return acc;
+    }
+}
